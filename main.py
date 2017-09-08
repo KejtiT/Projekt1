17e335898d7f40987f9bc3bc98447c56b99b030c
KejtiK <kattro2103@gmail.com>
2017-09-08 14:42:27 +0200

    bm

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
 def main(msg):
-<<<<<<< HEAD
+
     print(msg)
 
 main("Hello World")
-=======
+
 	print(msg)
 
 def secondfunction():
@@ -13,4 +13,3 @@ def secondfunction():
 
 
 main("Hello People")
->>>>>>> f7ca546303b7e6c26c2233dabf3e6e14739e5ee7
